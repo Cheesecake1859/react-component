367260dff9cd7d51126b736d93250549382fbb01
Saw Min Thant <sawminthant57@gmail.com>
2025-12-19 15:50:55 +0700

    Add react component

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import './App.css'
+import Greetings from './Greetings.jsx';
+import User from './user.jsx';
+ 
+function App() {
+  return (
+    <div className="card">
+      <Greetings />
+      <User name="Saw Min Thant" />
+    </div>
+  );
+}
+ 
+export default App
+ 
+ 
---
+++ src/Greetings.jsx
@@ -0,0 +1,7 @@
+function Greetings() {
+    return (
+      <h1>Hello React!</h1>
+    );
+  }
+  
+  export default Greetings;
